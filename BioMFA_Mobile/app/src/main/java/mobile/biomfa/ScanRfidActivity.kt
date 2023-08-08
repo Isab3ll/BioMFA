ffe306dc5aaf69465be4da2fcc7a294a45dbc2c3
Isab3ll <izamarczewska0@gmail.com>
2023-08-09 01:27:42 +0200

    First attempt for reading NFC tags

--- BioMFA_Mobile/app/src/main/java/mobile/biomfa/ScanRfidActivity.kt
+++ BioMFA_Mobile/app/src/main/java/mobile/biomfa/ScanRfidActivity.kt
@@ -1,15 +1,22 @@
 package mobile.biomfa
 
+import android.app.PendingIntent
+import android.content.Intent
 import android.content.res.ColorStateList
 import android.graphics.Color
-import android.os.Bundle
-import android.widget.Button
-import androidx.appcompat.app.AppCompatActivity
 import android.nfc.NfcAdapter
+import android.nfc.Tag
+import android.nfc.tech.Ndef
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class ScanRfidActivity : AppCompatActivity() {
 
     private var nfcAdapter: NfcAdapter? = null
+    private val nfcCallback = NfcAdapter.ReaderCallback { tag -> readFromTag(tag) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +26,49 @@ class ScanRfidActivity : AppCompatActivity() {
         returnButton.backgroundTintList = ColorStateList.valueOf(Color.parseColor("#363636"))
         returnButton.setOnClickListener {
             finish()
+        }
+
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        nfcAdapter?.enableReaderMode(this, nfcCallback, NfcAdapter.FLAG_READER_NFC_A or NfcAdapter.FLAG_READER_SKIP_NDEF_CHECK, null)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        nfcAdapter?.disableReaderMode(this)
+    }
+
+    private fun readFromTag(tag: Tag) {
+        val ndef = Ndef.get(tag)
+
+        try {
+            ndef?.connect()
+            val ndefMessage = ndef?.ndefMessage
+            if (ndefMessage != null) {
+                val records = ndefMessage.records
+                if (records.isNotEmpty()) {
+                    val payload = records[0].payload
+                    val text = String(payload, Charsets.UTF_8)
+
+                    val showDataIntent = Intent(this, ShowDataActivity::class.java).apply {
+                        putExtra(ShowDataActivity.EXTRA_SCANNED_DATA, text)
+                        putExtra(ShowDataActivity.EXTRA_TAG_TYPE, "NFC")
+                    }
+                    startActivity(showDataIntent)
+                }
+            } else {
+                runOnUiThread {
+                    Toast.makeText(applicationContext, "No NDEF records found on the tag.", Toast.LENGTH_LONG).show()
+                }
+            }
+            ndef?.close()
+        } catch (e: Exception) {
+            runOnUiThread {
+                Toast.makeText(applicationContext, "Cannot Read From Tag.", Toast.LENGTH_LONG).show()
+            }
         }
     }
 
---
+++ BioMFA_Mobile/app/src/main/java/mobile/biomfa/ShowDataActivity.kt
@@ -0,0 +1,39 @@
+package mobile.biomfa
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class ShowDataActivity : AppCompatActivity() {
+
+    companion object {
+        const val EXTRA_SCANNED_DATA = "extra_scanned_data"
+        const val EXTRA_TAG_TYPE = "extra_tag_type"
+    }
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_data)
+
+        val dataTextView: TextView = findViewById(R.id.data_textview)
+        val tagTypeTextView: TextView = findViewById(R.id.tag_type_textview)
+
+        val scannedData = intent.getStringExtra(EXTRA_SCANNED_DATA)
+        val tagType = intent.getStringExtra(EXTRA_TAG_TYPE)
+
+        if (scannedData != null) {
+            dataTextView.text = scannedData
+        } else {
+            dataTextView.text = "No data found."
+        }
+
+        if (tagType != null) {
+            tagTypeTextView.text = "Tag Type: $tagType"
+        } else {
+            tagTypeTextView.text = "Tag Type: Unknown"
+        }
+    }
+}
+
