456b1bbf24db40b5d29604bd812504cb00d65eee
Isab3ll <izamarczewska0@gmail.com>
2023-10-21 21:24:44 +0200

    Added basic server and web app

--- BioMFA_Server/server.py
+++ BioMFA_Server/server.py
@@ -1,68 +1,164 @@
-import socket
+import asyncio
+import websockets
+import json
+import sqlite3
 import random
+import redis
 
-# Adres i port serwera
-HOST = '127.0.0.1'
-PORT = 12345
+# Połączenie z bazą SQLite (User)
+sqlite_conn = sqlite3.connect('users.db')
+sqlite_cursor = sqlite_conn.cursor()
 
-# Słownik do przechowywania danych z operacji REGISTER
-registered_data = {}
+# Połączenie z Redis (Operation)
+redis_conn = redis.StrictRedis(host='192.168.6.146', port=20646, db=0)
 
-# A - BioMFA_Mobile, aplikacja mobilna w Kotlinie
-# B - BioMFA_Web, aplikacja webowa w Pyhtonie
+# Funkcja obsługująca rejestrację użytkownika
+async def register_user(username, password, websocket):
+    # Zapisz dane użytkownika w bazie Operation (Redis)
+    operation_id = generate_operation_id()
+    operation_data = {
+        "username": username,
+        "password": password,
+        "web_addr": websocket.remote_address[0]
+    }
+    redis_conn.hset("Operation", operation_id, json.dumps(operation_data))
 
-# Funkcja do generowania jednorazowego kodu ID operacji
+    # Odpowiedz z ID operacji
+    response = {
+        "action": "REGISTER",
+        "sender": "Server",
+        "content": {
+            "operation_id": operation_id
+        }
+    }
+    await websocket.send(json.dumps(response))
+
+# Funkcja obsługująca logowanie użytkownika
+async def login_user(username, password, websocket):
+    # Weryfikacja hasła w bazie danych SQLite (User)
+    sqlite_cursor.execute("SELECT username, password FROM User WHERE username=?", (username,))
+    user_data = sqlite_cursor.fetchone()
+
+    if user_data is None or user_data[1] != password:
+        # Wysłanie komunikatu o błędzie logowania
+        response = {
+            "action": "LOGIN",
+            "sender": "Server",
+            "content": "Invalid credentials"
+        }
+        await websocket.send(json.dumps(response))
+    else:
+        # Logowanie udane, generowanie ID operacji
+        operation_id = generate_operation_id()
+        operation_data = {
+            "username": username,
+            "web_addr": websocket.remote_address[0]
+        }
+        redis_conn.hset("Operation", operation_id, json.dumps(operation_data))
+
+        # Odpowiedz z ID operacji
+        response = {
+            "action": "LOGIN",
+            "sender": "Server",
+            "content": {
+                "operation_id": operation_id
+            }
+        }
+        await websocket.send(json.dumps(response))
+
+# Funkcja generująca unikalne ID operacji
 def generate_operation_id():
     return str(random.randint(100000, 999999))
 
-# Funkcja do obsługi operacji REGISTER od aplikacji B
-def handle_register_from_B(client_socket):
-    operation_id = generate_operation_id()
-    registered_data[operation_id] = client_socket
-    client_socket.send(operation_id.encode())
+async def handle_client(websocket, path):
+    async for message in websocket:
+        message_data = json.loads(message)
+        action = message_data.get("action")
+        sender = message_data.get("sender")
+        content = message_data.get("content")
 
-# Funkcja do obsługi operacji REGISTER od aplikacji A
-def handle_register_from_A(client_socket, operation_id, mfa_id):
-    if operation_id in registered_data:
-        b_client_socket = registered_data[operation_id]
-        b_client_socket.send(mfa_id.encode())
+        if action == "REGISTER" and sender == "Web":
+            username = content.get("username")
+            password = content.get("password")
 
-# Funkcja do obsługi operacji LOGIN od aplikacji B
-def handle_login_from_B(client_socket):
-    operation_id = generate_operation_id()
-    registered_data[operation_id] = client_socket
-    client_socket.send(operation_id.encode())
+            await register_user(username, password, websocket)
 
-# Funkcja do obsługi operacji LOGIN od aplikacji A
-def handle_login_from_A(client_socket, operation_id, mfa_id):
-    if operation_id in registered_data:
-        b_client_socket = registered_data[operation_id]
-        b_client_socket.send(mfa_id.encode())
+        elif action == "LOGIN" and sender == "Web":
+            username = content.get("username")
+            password = content.get("password")
 
-# Główna funkcja obsługująca serwer
-def main():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
-        server_socket.bind((HOST, PORT))
-        server_socket.listen()
+            await login_user(username, password, websocket)
 
-        print(f"Serwer nasłuchuje na {HOST}:{PORT}")
+        elif action == "REGISTER" and sender == "Mobile":
+            operation_id = content.get("operation_id")
+            mfa_id = content.get("mfa_id")
 
-        while True:
-            client_socket, addr = server_socket.accept()
-            with client_socket:
-                print(f"Połączono z {addr}")
+            # Obsłuż rejestrację użytkownika z aplikacji mobilnej
+            # Dopisz mfa_id do odpowiedniego rekordu w bazie Operation
+            operation_data = json.loads(redis_conn.hget("Operation", operation_id))
+            operation_data["mfa_id"] = mfa_id
+            redis_conn.hset("Operation", operation_id, json.dumps(operation_data))
 
-                message = client_socket.recv(1024).decode()
-                if message == "REGISTER":
-                    handle_register_from_B(client_socket)
-                elif message.startswith("REGISTER "):
-                    _, operation_id, mfa_id = message.split()
-                    handle_register_from_A(client_socket, operation_id, mfa_id)
-                elif message == "LOGIN":
-                    handle_login_from_B(client_socket)
-                elif message.startswith("LOGIN "):
-                    _, operation_id, mfa_id = message.split()
-                    handle_login_from_A(client_socket, operation_id, mfa_id)
+            # Odczytaj adres Web, na który należy przesłać komunikat sukcesu
+            web_addr = operation_data.get("web_addr")
+            web_socket = await websockets.connect(f"ws://{web_addr}")
+            success_message = {
+                "action": "REGISTER",
+                "sender": "Server",
+                "content": "Registration successful"
+            }
+            await web_socket.send(json.dumps(success_message))
 
-if __name__ == "__main__":
-    main()
+            # Przenieś rekord Operation do bazy User, zatwierdzając użytkownika
+            user_data = {
+                "username": operation_data["username"],
+                "password": operation_data["password"],
+                "mfa_id": operation_data["mfa_id"]
+            }
+            sqlite_cursor.execute("INSERT INTO User (username, password, mfa_id) VALUES (?, ?, ?)",
+                                  (user_data["username"], user_data["password"], user_data["mfa_id"]))
+            sqlite_conn.commit()
+
+        elif action == "LOGIN" and sender == "Mobile":
+            operation_id = content.get("operation_id")
+            mfa_id = content.get("mfa_id")
+
+            # Sprawdź, czy ID operacji istnieje w bazie Operation
+            if redis_conn.hexists("Operation", operation_id):
+                operation_data = json.loads(redis_conn.hget("Operation", operation_id))
+
+                # Sprawdź poprawność MFA ID w bazie User
+                if operation_data.get("mfa_id") == mfa_id:
+                    # Odczytaj adres Web
+                    web_addr = operation_data.get("web_addr")
+
+                    # Odpowiedz wynikiem operacji na odczytany adres Web
+                    web_socket = await websockets.connect(f"ws://{web_addr}")
+                    success_message = {
+                        "action": "LOGIN",
+                        "sender": "Server",
+                        "content": "Login successful"
+                    }
+                    await web_socket.send(json.dumps(success_message))
+                else:
+                    # Odpowiedz błędem autoryzacji MFA
+                    failure_message = {
+                        "action": "LOGIN",
+                        "sender": "Server",
+                        "content": "MFA authentication failed"
+                    }
+                    await websocket.send(json.dumps(failure_message))
+            else:
+                # Odpowiedz błędem braku operacji
+                failure_message = {
+                    "action": "LOGIN",
+                    "sender": "Server",
+                    "content": "Operation not found"
+                }
+                await websocket.send(json.dumps(failure_message))
+
+start_server = websockets.serve(handle_client, "192.168.6.146", 20646)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
+
---
+++ BioMFA_Web/web_app.py
@@ -0,0 +1,58 @@
+import asyncio
+import websockets
+import json
+
+# Zmienna do przechowywania informacji o zarejestrowanych użytkownikach
+registered_users = {}
+
+# Zmienna do przechowywania informacji o użytkownikach zalogowanych do MFA
+logged_in_users = {}
+
+# Adres serwera WebSocket
+server_address = "ws://192.168.6.146:30646"
+
+# Funkcja do obsługi rejestracji
+async def register(username, password, websocket):
+    if username in registered_users:
+        return {"action": "REGISTER", "content": "Username already exists."}
+    registered_users[username] = password
+    return {"action": "REGISTER", "content": "Registration successful."}
+
+# Funkcja do obsługi logowania
+async def login(username, password, websocket):
+    if username not in registered_users or registered_users[username] != password:
+        return {"action": "LOGIN", "content": "Invalid credentials."}
+    # Logowanie udane, generowanie MFA ID (to tylko przykład)
+    mfa_id = username + "_mfa"
+    logged_in_users[username] = mfa_id
+    return {"action": "LOGIN", "content": mfa_id}
+
+async def main():
+    async with websockets.connect(server_address) as websocket:
+        while True:
+            print("1. Register")
+            print("2. Login")
+            choice = input("Choose an action (1/2): ")
+
+            if choice == "1":
+                username = input("Enter username: ")
+                password = input("Enter password: ")
+                message = {"action": "REGISTER", "content": {"username": username, "password": password}}
+                await websocket.send(json.dumps(message))
+                response = await websocket.recv()
+                print(response)
+
+            elif choice == "2":
+                username = input("Enter username: ")
+                password = input("Enter password: ")
+                message = {"action": "LOGIN", "content": {"username": username, "password": password}}
+                await websocket.send(json.dumps(message))
+                response = await websocket.recv()
+                response_data = json.loads(response)
+                if response_data.get("action") == "LOGIN" and not response_data.get("content").startswith("Invalid"):
+                    mfa_id = response_data.get("content")
+                    print(f"Logged in. Your MFA ID: {mfa_id}")
+                    # Tutaj możesz wykonywać operacje związane z zalogowanym użytkownikiem
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(main())
